b4b41997c885239b7ef0fd386a3710ee8ea0d6a6
Ioann Volkov <volkov.ioann@gmail.com>
2021-08-24 23:02:06 +0300

    Improved scripts for testing UDP stability

--- test_udp/receive_udp.py
+++ test_udp/receive_udp.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import socket
+from datetime import datetime
 
-UDP_IP = "192.168.199.2"  # your local IP
+UDP_IP = '192.168.199.2'  # your local IP
 UDP_PORT = 25062
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,4 +11,4 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024)
-    print("received message [%s] from [%s]" % (data, addr))
+    print('[%s] Received message [%s] from [%s]' % (datetime.now().strftime('%d.%m.%Y %H:%M:%S'), data, addr))
--- test_udp/send_udp_multicast.py
+++ test_udp/send_udp_multicast.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+import time
 
 MCAST_GRP = '227.16.119.203'
 MCAST_PORT = 25061
@@ -10,4 +11,6 @@ MULTICAST_TTL = 2
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
 
-sock.sendto(b'SMART_HOME_SCAN', (MCAST_GRP, MCAST_PORT))
+while True:
+    sock.sendto(b'SMART_HOME_SCAN', (MCAST_GRP, MCAST_PORT))
+    time.sleep(30)
