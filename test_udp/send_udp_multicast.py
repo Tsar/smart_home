3fb23da0edb5735c0793d4186d35a911b036562a
Ioann Volkov <volkov.ioann@gmail.com>
2021-08-25 21:41:28 +0300

    Sender test UDP script: logging when sending

--- test_udp/send_udp_multicast.py
+++ test_udp/send_udp_multicast.py
@@ -2,6 +2,7 @@
 
 import socket
 import time
+from datetime import datetime
 
 MCAST_GRP = '227.16.119.203'
 MCAST_PORT = 25061
@@ -13,4 +14,5 @@ sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
 
 while True:
     sock.sendto(b'SMART_HOME_SCAN', (MCAST_GRP, MCAST_PORT))
+    print('[%s] Sent SMART_HOME_SCAN' % datetime.now().strftime('%d.%m.%Y %H:%M:%S'))
     time.sleep(30)
