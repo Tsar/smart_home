869273bdd454b285d67febc465dd360c7d59ff53
Ioann Volkov <volkov.ioann@gmail.com>
2021-12-14 16:43:43 +0300

    Alice: Intermediate stage of new implementation for Android Find

--- Voice_Integration/google_android_find.py
+++ Voice_Integration/google_android_find.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import time
 import json
+import threading
 import urllib.request
+import urllib.parse
+import http.cookiejar
 
 class NoParamsException(Exception):
     pass
@@ -12,19 +16,71 @@ class FailedToStartRingingException(Exception):
 class UnexpectedResponse(Exception):
     pass
 
-def ringPhone(phoneId):
-    with open('phone_find_queries.json', 'r') as phoneFindFile:
-        phoneFindQueries = json.loads(phoneFindFile.read())
-    if phoneId not in phoneFindQueries:
-        raise NoParamsException()
+params = {}
+cookies = {}
+cookiesLock = threading.Lock()
 
-    phoneFindQuery = phoneFindQueries[phoneId]
-    try:
-        request = urllib.request.Request(phoneFindQuery['url'], headers=phoneFindQuery['headers'], data=phoneFindQuery['post_data'].encode('UTF-8'))
-        response = urllib.request.urlopen(request, timeout=1.5).read()
-    except Exception as err:
-        raise FailedToStartRingingException(err)
+COOKIES_UPDATE_INTERVAL_SECONDS = 3600  # 1 hour
+
+def _cookieUpdaterRoutine():
+    global cookies, cookiesLock
+
+    while True:
+        # TODO
+        time.sleep(COOKIES_UPDATE_INTERVAL_SECONDS)
+
+def init(launchCookieUpdaterRoutine=True):
+    global params, cookies
+
+    with open('google_android_find_params.json', 'r') as paramsFile:
+        params = json.loads(paramsFile.read())
+        for phoneParams in params.values():
+            assert 'cookie_id' in phoneParams
+            assert 'post_data' in phoneParams
+            cookieId = phoneParams['cookie_id']
+            cookies[cookieId] = http.cookiejar.MozillaCookieJar('google_cookies/%s.txt' % cookieId)
+
+    if launchCookieUpdaterRoutine:
+        threading.Thread(target=_cookieUpdaterRoutine, daemon=True).start()
+
+def ringPhone(phoneId):
+    global cookies, cookiesLock
+
+    if phoneId not in params:
+        raise NoParamsException()
+    phoneParams = params[phoneId]
+
+    with cookiesLock:
+        if phoneParams['cookie_id'] not in cookies:
+            raise NoParamsException()
+        cookie = cookies[phoneParams['cookie_id']]
+        cookie.load(ignore_expires=True)
+
+        xtdc = None
+        for cookieElement in cookie:
+            if cookieElement.name == 'xtdc':
+                xtdc = cookieElement.value
+                break
+        if xtdc is None:
+            raise NoParamsException()
+
+        try:
+            opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+            request = urllib.request.Request(
+                'https://www.google.com/android/find/xhr/dc/stopRinging?xtdc=%s' % urllib.parse.quote_plus(xtdc),
+                headers={'content-type': 'application/x-www-form-urlencoded;charset=UTF-8'},
+                data=phoneParams['post_data'].encode('UTF-8')
+            )
+            response = opener.open(request, timeout=1.5).read()
+        except Exception as err:
+            raise FailedToStartRingingException(err)
+
+        cookie.save(ignore_expires=True)
 
     respStr = response.decode('UTF-8')
-    if respStr != phoneFindQuery['expected_response']:
+    if respStr != ")]}'\n[]":
         raise UnexpectedResponse(respStr)
+
+if __name__ == "__main__":
+    init(launchCookieUpdaterRoutine=False)
+    ringPhone('tsar_phone')
